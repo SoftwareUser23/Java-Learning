3208862de5b9cc68b3cace8c864c9b2587642418
SoftwareUser2three <69643038+SoftwareUser2three@users.noreply.github.com>
2020-11-23 23:32:11 +0530

    update

--- src/com/company/video31.java
+++ src/com/company/video31.java
@@ -2,14 +2,30 @@ package com.company;
 // Methods in Java
 public class video31 {
     public static void main(String[] args) {
-int a,b,c;
-c = 0;
-b =9;
-boolean o = false;
-for (int d=0;d<c;d++)
-{
-    
-}
+        int a = 51;
+        int b = 17;
+        int c;
+        c = logic(a, b);
+        System.out.println(a + " "+ b);
+        System.out.println("Next Values\t");
+        int a1 = 12;
+        int b1 = 11;
+        int c1;
+        c1 = logic(a1, b1);
+        System.out.println(c);
+        System.out.println(c1);
+    }
+    static int logic(int x, int y){
+        int z;
+        if(x>y){
+            z = x+y;
+        }
+        else {
+            z = (x +y) * 15;
+        }
+        x = 226;
+        return z;
+    }
 
 }
 
